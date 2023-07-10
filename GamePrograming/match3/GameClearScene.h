215fb95c5af3d9fab4f7fd500c9dd270880b5c8c
shiona0820 <138412503+shiona0820@users.noreply.github.com>
2023-07-10 11:25:02 +0900

    GameClearScene.h追加

---
+++ GamePrograming/match3/GameClearScene.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+*マクロ定義
+**/
+
+/**
+*型定義
+**/
+
+
+/**
+*プロトタイプ宣言
+**/
+
+int GameClearScene_Initialize(void);
+void GameClearScene_Update(void);
+void GameClearScene_Draw(void);
